a476f09d5323d831f7969be460ec1b84b23e53fb
illiagitdev <illya.posv.b@gmail.com>
2020-06-06 18:18:21 +0300

    Add parsing model for custom search (basic information)

---
+++ src/main/java/com/illia/riasurfing/entities/search/searchid/IdSearchResponseSlim.java
@@ -0,0 +1,23 @@
+package com.illia.riasurfing.entities.search.searchid;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode()
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class IdSearchResponseSlim {
+    private int userId;
+    private String locationCityName;
+    @JsonProperty("USD")
+    private int usd;
+    private String markName;
+    private String modelName;
+    private PhotoData photoData;
+    private String linkToView;
+    private String title;
+}
