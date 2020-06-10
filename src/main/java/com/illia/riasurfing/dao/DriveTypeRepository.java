b192059c3683ba56553622922310f1742ecea217
illiagitdev <illya.posv.b@gmail.com>
2020-06-11 01:57:16 +0300

    Add class and repository interface for drive type

---
+++ src/main/java/com/illia/riasurfing/dao/DriveTypeRepository.java
@@ -0,0 +1,14 @@
+package com.illia.riasurfing.dao;
+
+import com.illia.riasurfing.entities.search.request.DriveType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DriveTypeRepository extends JpaRepository<DriveType, Integer> {
+    @Query("FROM DriveType WHERE category_id=?1")
+    List<DriveType> findByCategory(int categoryId);
+}
---
+++ src/main/java/com/illia/riasurfing/entities/search/request/DriveType.java
@@ -0,0 +1,38 @@
+package com.illia.riasurfing.entities.search.request;
+
+import com.illia.riasurfing.entities.BaseEntity;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Component
+@Entity
+@Table(name = "drive_types")
+public class DriveType extends BaseEntity {
+    private String name;
+    private int value;
+
+    public DriveType() {
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "value")
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
