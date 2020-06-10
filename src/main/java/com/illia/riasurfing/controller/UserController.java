1c5a9dad1e6dadaf964295d702e4d87c8d3a3e6c
illiagitdev <illya.posv.b@gmail.com>
2020-06-11 01:14:01 +0300

    Add endpoint: return search history for logged in user

--- src/main/java/com/illia/riasurfing/controller/UserController.java
+++ src/main/java/com/illia/riasurfing/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.illia.riasurfing.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.illia.riasurfing.dao.CustomRequestRepository;
 import com.illia.riasurfing.entities.User;
+import com.illia.riasurfing.entities.search.request.CustomRequest;
 import com.illia.riasurfing.service.UserService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,26 +13,35 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
     private UserService userService;
+    private CustomRequestRepository requestRepository;
+    private ObjectMapper mapper;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, CustomRequestRepository requestRepository) {
         this.userService = userService;
+        this.requestRepository = requestRepository;
     }
 
-    @GetMapping(path = "/{nickname}")
-    public ResponseEntity<?> showDetails(@PathVariable("nickname") String nickname) {
-        return ResponseEntity.ok(userService.getUser(nickname));
+    @Autowired
+    public void setMapper(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<?> showDetails() {
+        return ResponseEntity.ok(userService.getUser(getUserId()));
     }
 
     @DeleteMapping
     public ResponseEntity<?> deleteUser() {
-        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        final String principalName = principal.getUsername();
-        userService.delete(userService.getUser(principalName).getId());
+        final int userId = getUserId();
+        userService.delete(userId);
         SecurityContextHolder.clearContext();
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User deleted");
     }
@@ -39,5 +51,18 @@ public class UserController {
     public ResponseEntity<?> updateUser(@RequestBody User jsonUser) {
         userService.update(jsonUser);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("User updated");
+    }
+
+    @SneakyThrows
+    @GetMapping(path = "/searchHistory")
+    public ResponseEntity<?> getSearchHistory() {
+        List<CustomRequest> requests = requestRepository.getAllByUserId(getUserId());
+        return ResponseEntity.ok(mapper.writeValueAsString(requests));
+    }
+
+    private int getUserId() {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String principalName = principal.getUsername();
+        return userService.getUser(principalName).getId();
     }
 }
