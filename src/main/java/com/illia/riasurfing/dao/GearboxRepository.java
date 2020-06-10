353b6db06ed37f5e6eb9dab363c9972ea660ca83
illiagitdev <illya.posv.b@gmail.com>
2020-06-11 01:58:25 +0300

    Add class and repository interface for auto transmission

---
+++ src/main/java/com/illia/riasurfing/dao/GearboxRepository.java
@@ -0,0 +1,14 @@
+package com.illia.riasurfing.dao;
+
+import com.illia.riasurfing.entities.search.request.GearBox;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GearboxRepository extends JpaRepository<GearBox, Integer> {
+    @Query("FROM GearBox WHERE category_id=?1")
+    List<GearBox> findByCategory(int categoryId);
+}
