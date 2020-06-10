fc50b604e8f97eeb2820b1bf3dbc1d405470d4b6
illiagitdev <illya.posv.b@gmail.com>
2020-06-11 01:21:04 +0300

    Add class and repository interface for auto brands

---
+++ src/main/java/com/illia/riasurfing/dao/AutoBrandRepository.java
@@ -0,0 +1,14 @@
+package com.illia.riasurfing.dao;
+
+import com.illia.riasurfing.entities.search.request.AutoBrand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AutoBrandRepository extends JpaRepository<AutoBrand, Integer> {
+    @Query("FROM AutoBrand WHERE category_id=?1")
+    List<AutoBrand> getAllByCategory(int id);
+}
---
+++ src/main/java/com/illia/riasurfing/entities/search/request/AutoBrand.java
@@ -0,0 +1,37 @@
+package com.illia.riasurfing.entities.search.request;
+
+import com.illia.riasurfing.entities.BaseEntity;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Component
+@Entity
+@Table(name = "auto_marks")
+public class AutoBrand extends BaseEntity {
+    private String name;
+    private int value;
+
+    AutoBrand() {
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "value")
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
