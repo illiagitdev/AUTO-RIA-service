2ae2613463cb9a0f4547f2a4cf833412b64bb55c
illiagitdev <illya.posv.b@gmail.com>
2020-06-11 01:21:29 +0300

    Add class and repository interface for auto model

---
+++ src/main/java/com/illia/riasurfing/dao/AutoModelRepository.java
@@ -0,0 +1,14 @@
+package com.illia.riasurfing.dao;
+
+import com.illia.riasurfing.entities.search.request.AutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AutoModelRepository extends JpaRepository<AutoModel, Integer> {
+    @Query("FROM AutoModel WHERE category_id=?1 AND mark_id=?2")
+    List<AutoModel> findByCategoryAndMark(int categoryId, int markaId);
+}
