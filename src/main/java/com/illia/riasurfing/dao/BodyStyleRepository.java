6057e2fa90e26441083a454c6456c96269e7b710
illiagitdev <illya.posv.b@gmail.com>
2020-06-11 01:21:59 +0300

    Add class and repository interface for auto bodystyle

---
+++ src/main/java/com/illia/riasurfing/dao/BodyStyleRepository.java
@@ -0,0 +1,15 @@
+package com.illia.riasurfing.dao;
+
+import com.illia.riasurfing.entities.search.request.BodyStyle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BodyStyleRepository extends JpaRepository<BodyStyle, Integer> {
+
+    @Query("FROM BodyStyle WHERE category_id=?1")
+    List<BodyStyle> getAllByCategory(int id);
+}
