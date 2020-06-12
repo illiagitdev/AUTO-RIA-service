1859c2e5887033374632c4318f26d2a8933f77f4
illiagitdev <illya.posv.b@gmail.com>
2020-06-12 12:40:35 +0300

    Update class CustomRequestRepository as PagingAndSortingRepository

--- src/main/java/com/illia/riasurfing/dao/CustomRequestRepository.java
+++ src/main/java/com/illia/riasurfing/dao/CustomRequestRepository.java
@@ -1,12 +1,15 @@
 package com.illia.riasurfing.dao;
 
 import com.illia.riasurfing.entities.search.request.CustomRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
-public interface CustomRequestRepository extends JpaRepository<CustomRequest, Integer> {
-    List<CustomRequest> getAllByUserId(int userId);
+public interface CustomRequestRepository extends PagingAndSortingRepository<CustomRequest, Integer> {
+
+    Page<CustomRequest> getByUserId(int userId, Pageable p);
+
 }
