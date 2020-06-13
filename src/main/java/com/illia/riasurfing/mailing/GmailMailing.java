4e5112cf4a716ed51904e9d5fddb6ec457382623
illiagitdev <illya.posv.b@gmail.com>
2020-06-14 00:38:02 +0300

    Add gmail mailing service

---
+++ src/main/java/com/illia/riasurfing/mailing/GmailMailing.java
@@ -0,0 +1,34 @@
+package com.illia.riasurfing.mailing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+public class GmailMailing implements MailingService {
+    private JavaMailSender javaMailSender;
+
+    public GmailMailing() {
+    }
+
+    @Autowired
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public synchronized void sendEmail(String from, String to, String subject, String text) throws MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.setSubject(subject);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setText(text, true);
+        javaMailSender.send(message);
+
+    }
+
+}
--- src/main/java/com/illia/riasurfing/mailing/MailingService.java
+++ src/main/java/com/illia/riasurfing/mailing/MailingService.java
@@ -2,9 +2,11 @@ package com.illia.riasurfing.mailing;
 
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 @Service
 public interface MailingService {
 
-    void sendEmail(String[] to, String subject, String text);
+    void sendEmail(String from, String to, String subject, String text) throws MessagingException;
 
 }
