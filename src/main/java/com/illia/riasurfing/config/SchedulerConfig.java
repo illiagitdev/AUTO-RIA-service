20b1cd19ea9fbb89abb9df06754750b2475fda2e
illiagitdev <illya.posv.b@gmail.com>
2020-06-16 10:39:25 +0300

    Update - delete bean that cause exception at authentication to host

--- src/main/java/com/illia/riasurfing/config/SchedulerConfig.java
+++ src/main/java/com/illia/riasurfing/config/SchedulerConfig.java
@@ -1,9 +1,6 @@
 package com.illia.riasurfing.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
@@ -11,11 +8,6 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
     private final int POOL_SIZE = 10;
-
-    @Bean
-    public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
-    }
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
