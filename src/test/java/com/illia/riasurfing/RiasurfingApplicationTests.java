20913a204aa52f30bb77076b3a93833ce9242baa
illiagitdev <illya.posv.b@gmail.com>
2020-06-20 18:23:12 +0300

    Add config class

--- src/test/java/com/illia/riasurfing/RiasurfingApplicationTests.java
+++ src/test/java/com/illia/riasurfing/RiasurfingApplicationTests.java
@@ -2,12 +2,17 @@ package com.illia.riasurfing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
+@TestPropertySource({"classpath:application-test.properties"})
 class RiasurfingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
