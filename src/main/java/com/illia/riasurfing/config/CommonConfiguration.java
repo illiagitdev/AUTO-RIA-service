e0c9f846a4c62fd9e2dc3e53dc0ec57f61bc1089
illiagitdev <illya.posv.b@gmail.com>
2020-06-21 18:40:24 +0300

    Update config class

--- src/main/java/com/illia/riasurfing/config/CommonConfiguration.java
+++ src/main/java/com/illia/riasurfing/config/CommonConfiguration.java
@@ -1,12 +1,8 @@
 package com.illia.riasurfing.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.illia.riasurfing.mailing.AmazonSES;
-import com.illia.riasurfing.mailing.GmailMailing;
-import com.illia.riasurfing.mailing.MailingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class CommonConfiguration {
@@ -14,18 +10,6 @@ public class CommonConfiguration {
     @Bean
     public ObjectMapper getMapper() {
         return new ObjectMapper();
-    }
-
-    @Bean
-    @Profile("aws")
-    public MailingService mailingServiceAWS() {
-        return new AmazonSES();
-    }
-
-    @Bean
-    @Profile("gmail")
-    public MailingService mailingServiceGmail() {
-        return new GmailMailing();
     }
 
 }
--- src/main/java/com/illia/riasurfing/mailing/GmailMailing.java
+++ src/main/java/com/illia/riasurfing/mailing/GmailMailing.java
@@ -3,10 +3,12 @@ package com.illia.riasurfing.mailing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+@Component
 public class GmailMailing implements MailingService {
     private JavaMailSender javaMailSender;
 
